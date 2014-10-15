5aaa1e685a5a082ad68cb2c998cb8edf9804749c
Lenny and Ryan <pair+lenny+rloomba@sharethrough.com>
2014-10-15 14:00:44 -0700

    add SDK to new sample app

--- src/main/java/com/sharethrough/sample/MyActivity.java
+++ src/main/java/com/sharethrough/sample/MyActivity.java
@@ -2,6 +2,8 @@ package com.sharethrough.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.sharethrough.sdk.Test;
 
 public class MyActivity extends Activity {
 
@@ -9,5 +11,7 @@ public class MyActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_my);
+
+    ((TextView)findViewById(R.id.text)).setText(Test.MESSAGE);
   }
 }
