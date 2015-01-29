5bc974fe22abe79cccec8da551e88cdf154c72ac
Ryan Loomba <engineers+rloomba@sharethrough.com>
2015-01-29 10:56:20 -0800

    update build tools to 21.1.1 to support multi dexing

--- src/main/java/com/sharethrough/SampleApplication.java
+++ src/main/java/com/sharethrough/SampleApplication.java
@@ -1,7 +1,18 @@
 package com.sharethrough;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 public class SampleApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        // Issue wiht Robolectric and Multi Dex support: https://github.com/robolectric/robolectric/issues/1328
+        try {
+            super.attachBaseContext(base);
+            MultiDex.install(this);
+        } catch (RuntimeException ignored) {
 
+        }
+    }
 }
